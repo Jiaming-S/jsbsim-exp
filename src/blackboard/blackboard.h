b3ac870580283c8079fbb665630152aab9520ee3
JMST <jiamingsitu06@gmail.com>
2026-08-05 22:13:39 -0700

    Msgpack serialization mixin, blocked temporarily

--- src/blackboard/blackboard.h
+++ src/blackboard/blackboard.h
@@ -7,3 +7,23 @@ class Blackboard {
   public:
     virtual ~Blackboard() = default;
 };
+
+// Mixin for serialization
+// TODO: AircraftHandle is on blackboard, and I don't want to
+// have to figure out how to serialize that...
+template <typename BBType> 
+class SerializableMixin : public Blackboard {
+  public:
+    // Serialization
+    msgpack::sbuffer pack() const {
+      msgpack::sbuffer sbuf;
+      msgpack::pack(sbuf, *static_cast<BBType*>(this));
+      return sbuf;
+    }
+
+    // Deserialization
+    void unpack(const char* data, const size_t size) const {
+      msgpack::object_handle oh = msgpack::unpack(data, size);
+      oh.get().convert(*static_cast<BBType*>(this));
+    }
+};
